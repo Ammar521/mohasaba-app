6a4ba214ea4dc4a2b4bda73276dfdc00b585d885
Ammar521 <142670645+Ammar521@users.noreply.github.com>
2026-05-31 19:55:51 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME="app-v2";
+const urls=["/","index.html"];
+
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urls)));
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
